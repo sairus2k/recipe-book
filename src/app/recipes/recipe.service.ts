c87f89ddb07f9a3ec4cfdda7b75acc40ae635722
Anton Savoskin <anton@sliday.com>
2017-01-07 11:35:23 +0700

    Cross service communication

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,10 +1,14 @@
 import { Injectable } from '@angular/core';
 import { Recipe } from '../recipe';
+import { Ingredient } from '../ingredient';
 
 @Injectable()
 export class RecipeService {
   private recipes: Recipe[] = [
-    new Recipe('Schnitzel', 'Very tasty', 'http://lorempixel.com/200/200/cats/1'),
+    new Recipe('Schnitzel', 'Very tasty', 'http://lorempixel.com/200/200/cats/1', [
+      new Ingredient('French Fries', 2),
+      new Ingredient('Pork Meat', 1)
+    ]),
     new Recipe('Summer Salad', 'Okayish', 'http://lorempixel.com/200/200/cats/2')
   ];
   constructor() { }
